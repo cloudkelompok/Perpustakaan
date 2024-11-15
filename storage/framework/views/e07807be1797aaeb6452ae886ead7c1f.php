dbfa6f5e95d959fbc6bf79a9b6982ba3ff17f013
cloudkelompok <cloudkelompok59@gmail.com>
2024-11-16 07:55:03 +0800

    Delete storage directory
    missing file

--- storage/framework/views/e07807be1797aaeb6452ae886ead7c1f.php
+++
@@ -1,7 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-    <h2>Identitas</h2>
-    <p>Halo, nama saya <strong><?php echo e($nama); ?></strong>,
-    saya tinggal di <strong><?php echo e($alamat); ?></strong></p>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('m4.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\perpus\resources\views/m4/identitas.blade.php ENDPATH**/ ?>
